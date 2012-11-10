ec66d842ddb87956a16233d022975bfa5069e7f8
Hamid Mamdoohi <hamid.mamdoohi@gmail.com>
2012-11-10 13:19:41 +0330

    ehau theme is added

--- app/View/Themed/Admin.Bootstrap/webroot/js/init.js
+++ app/View/Themed/Admin.Bootstrap/webroot/js/init.js
@@ -20,6 +20,28 @@ function alert(message) {
 		}
 	})
 }
+$(function(){
+    // fix sub nav on scroll
+    var $win = $(window)
+      , $nav = $('#toolbar-menu')
+      , navTop = $('#toolbar-menu').length && $('#toolbar-menu').offset().top - 40
+      , isFixed = 0
+
+    processScroll()
+
+    $win.on('scroll', processScroll)
+
+    function processScroll() {
+      var i, scrollTop = $win.scrollTop()
+      if (scrollTop >= navTop && !isFixed) {
+        isFixed = 1
+        $nav.addClass('toolbar-menu-fixed')
+      } else if (scrollTop <= navTop && isFixed) {
+        isFixed = 0
+        $nav.removeClass('toolbar-menu-fixed')
+      }
+    }
+})
 /*
 function confirm(message) {
 	var flag = true;
